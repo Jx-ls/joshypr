f5241b740a06a77f204ed1027f4ff1402a3d8ecc
Jx-ls <joshuarajtadi@gmail.com>
2025-09-14 03:39:29 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-ln -sf ~/.dots/.vimrc ~/.vimrc
+echo "Linking dotfiles..."
+
+CONFIGS=(
+  dunst
+  gtk-3.0
+  hypr
+  kitty
+  qt5ct
+  qt6ct
+  ranger
+  rofi
+  spicetify
+  Thunar
+  waybar
+  zsh
+)
+
+for config in "${CONFIGS[@]}"; do
+  echo "Linking $config..."
+  rm -rf "$HOME/.config/$config"
+  ln -sf "$HOME/.dots/$config" "$HOME/.config/$config"
+done
+
+# Link individual files
+echo "Linking standalone files..."
+ln -sf ~/.dots/.tmux.conf ~/
+ln -sf ~/.dots/.vimrc ~/
+ln -sf ~/.dots/.zshrc ~/
+
+echo "All done."
+
